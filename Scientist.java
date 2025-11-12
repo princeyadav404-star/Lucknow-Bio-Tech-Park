8bffa18adf585ff2ede7a25fd41efe18557e221c
princeyadav404-star <princeyadavprinceyadav404@gmail.com>
2025-11-12 19:20:04 +0530

    Add files via upload

---
+++ Scientist.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Scientist {
+    private int id;
+    private String name;
+    private String specialization;
+
+    public Scientist() {}
+    public Scientist(int id, String name, String specialization) {
+        this.id = id; this.name = name; this.specialization = specialization;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getSpecialization() { return specialization; }
+    public void setSpecialization(String specialization) { this.specialization = specialization; }
+}
